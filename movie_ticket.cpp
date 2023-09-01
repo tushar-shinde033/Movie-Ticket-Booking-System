27000cfa9a3561af34b4bad036754a605e549fce
tushar shinde033 <tusharshinde0330@gmail.com>
2023-09-01 11:52:05 +0530

    Second Commit

--- movie_ticket.cpp
+++ movie_ticket.cpp
@@ -7,16 +7,21 @@ const int NUM_ROWS = 5;
 const int NUM_COLS = 5;
 const int SEAT_PRICE = 10;
 
-
 vector<vector<bool>> seats(NUM_ROWS, vector<bool>(NUM_COLS, false));
 
-void displaySeats() {
+void displaySeats()
+{
     cout << "Current Seat Availability:" << endl;
-    for (int row = 0; row < NUM_ROWS; ++row) {
-        for (int col = 0; col < NUM_COLS; ++col) {
-            if (seats[row][col]) {
+    for (int row = 0; row < NUM_ROWS; ++row)
+    {
+        for (int col = 0; col < NUM_COLS; ++col)
+        {
+            if (seats[row][col])
+            {
                 cout << " X ";
-            } else {
+            }
+            else
+            {
                 cout << " O ";
             }
         }
@@ -25,19 +30,23 @@ void displaySeats() {
     cout << endl;
 }
 
-
-int calculateTotalCost(const vector<pair<int, int>>& selectedSeats) {
+int calculateTotalCost(const vector<pair<int, int>> &selectedSeats)
+{
     int totalCost = 0;
-    for (const auto& seat : selectedSeats) {
+    for (const auto &seat : selectedSeats)
+    {
         totalCost += SEAT_PRICE;
     }
     return totalCost;
 }
 
-int main() {
+int main()
+{
+    cout << "-------------------------------------------------\n";
     cout << "Welcome to the Movie Ticket Booking System!" << endl;
 
-    while (true) {
+    while (true)
+    {
         displaySeats();
 
         cout << "1. View Movie Listings" << endl;
@@ -47,47 +56,57 @@ int main() {
         int choice;
         cin >> choice;
 
-        switch (choice) {
-            case 1: {
-                cout << "Movie Listings:" << endl;
-                cout << "1. Movie A" << endl;
-                cout << "2. Movie B" << endl;
+        switch (choice)
+        {
+        case 1:
+        {
+            cout << "------------------------\n";
+            cout << "Movie Listings:" << endl;
+            cout << "1. Movie A" << endl;
+            cout << "2. Movie B" << endl;
+            break;
+        }
+        case 2:
+        {
+            displaySeats();
+            vector<pair<int, int>> selectedSeats;
+            int row, col;
+
+            cout << "Enter the row and column of the seat you want to book: ";
+            cin >> row >> col;
+
+            if (row < 1 || row > NUM_ROWS || col < 1 || col > NUM_COLS)
+            {
+                cout << "Invalid seat selection !. Please try again." << endl;
                 break;
             }
-            case 2: {
-                displaySeats();
-                vector<pair<int, int>> selectedSeats;
-                int row, col;
 
-                cout << "Enter the row and column of the seat you want to book: ";
-                cin >> row >> col;
-
-                if (row < 1 || row > NUM_ROWS || col < 1 || col > NUM_COLS) {
-                    cout << "Invalid seat selection. Please try again." << endl;
-                    break;
-                }
-
-                if (seats[row - 1][col - 1]) {
-                    cout << "Seat is already booked. Please select another seat." << endl;
-                    break;
-                }
-
-                seats[row - 1][col - 1] = true;
-                selectedSeats.emplace_back(row, col);
-
-                int totalCost = calculateTotalCost(selectedSeats);
-                cout << "Seat booked successfully!" << endl;
-                cout << "Total Cost: $" << totalCost << endl;
+            if (seats[row - 1][col - 1])
+            {
+                cout << "Seat is already booked !. Please select another seat." << endl;
                 break;
             }
-            case 3: {
-                cout << "Thank you for using the Movie Ticket Booking System. Goodbye!" << endl;
-                return 0;
-            }
-            default: {
-                cout << "Invalid choice. Please select a valid option." << endl;
-                break;
-            }
+
+            seats[row - 1][col - 1] = true;
+            selectedSeats.emplace_back(row, col);
+
+            int totalCost = calculateTotalCost(selectedSeats);
+            cout << "Seat booked successfully!" << endl;
+            cout << "Total Cost: $" << totalCost << endl;
+            break;
+        }
+        case 3:
+        {
+            cout << "Thank you for using the Movie Ticket Booking System. Goodbye!" << endl;
+            cout << "-----------------------------------------------------------------" << endl;
+            return 0;
+        }
+        default:
+        {
+            cout << "Invalid choice. Please select a valid option." << endl;
+            cout << "-----------------------------------------------" << endl;
+            break;
+        }
         }
     }
 
